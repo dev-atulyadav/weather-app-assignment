8d5ba4b2d2f8cb07105b6e4506fc2d5bd54c841a
dev-atulyadav <atulyadav031103@gmail.com>
2024-04-11 12:07:12 +0530

    api key removed and loader fixed

--- src/components/Main/Info.jsx
+++ src/components/Main/Info.jsx
@@ -5,13 +5,20 @@ import WeatherReport from "./WeatherReport";
 import Forecast from "./Forecast";
 
 const Info = () => {
-  const { data, load } = useContext(CurrentDataContext);
+  const { data } = useContext(CurrentDataContext);
   return (
     <>
-      <section className="bg-black text-white p-4 pt-24 flex justify-center items-center flex-col w-full gap-4">
-        <WeatherReport data={data} />
-        <h2 className="text-3xl font-semibold border-b p-4 uppercase">8 Day Forecast</h2>
-        <Forecast />
+      <section className={`${data.location==undefined?"h-screen":""} bg-black text-white p-4 pt-24 flex justify-center items-center flex-col w-full gap-4`}>
+        {
+          data.location==undefined?<Loader/> :
+          <>
+            <WeatherReport data={data} />
+            <h2 className="text-3xl font-semibold border-b p-4 uppercase">
+              8 Day Forecast
+            </h2>
+            <Forecast />
+          </>
+        }
       </section>
     </>
   );
--- src/contexts/CurrentDataState.jsx
+++ src/contexts/CurrentDataState.jsx
@@ -5,26 +5,29 @@ import { SearchLocContext } from "./SearchLocState";
 export const CurrentDataContext = createContext();
 const CurrentDataState = ({ children }) => {
   const { loc } = useContext(SearchLocContext);
-  const [load, setLoad] = useState(false);
   const [search, setSearch] = useState(false);
   const [data, setData] = useState({});
   useEffect(() => {
-    let url = `http://api.weatherapi.com/v1/forecast.json?key=f8366378fb174ab183681435241004&q=${loc}&days=8`;
+    const key = import.meta.env.VITE_WEATHER_API_KEY;
+    let url = `http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${loc}&days=8`;
     let resp = axios.get(url);
-    resp.then((res) => {
-      setData(res.data);
-      if (data.weather === undefined) {
+    resp
+      .then((res) => {
         setTimeout(() => {
-          setLoad(!load);
-        }, 2000);
-      }
-    });
+          setData(res.data);
+        }, 1500);
+      })
+      .catch((err) => {
+        console.log(err.response.data.error.message);
+      });
   }, [search]);
   const handleSearch = (e) => {
+    setData({});
     setSearch(!search);
   };
+  console.log(data);
   return (
-    <CurrentDataContext.Provider value={{ data, load, search, handleSearch }}>
+    <CurrentDataContext.Provider value={{ data, search, handleSearch }}>
       {children}
     </CurrentDataContext.Provider>
   );
